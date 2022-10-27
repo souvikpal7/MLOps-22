8e92fe50e59dee4c82467e7a1dfaefea7afb8631
souvikpal7 <pal.18@iitj.ac.in>
2022-10-28 02:40:07 +0530

    added decision tree

--- hand_written_digits.py
+++ hand_written_digits.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from itertools import product
 from sklearn import svm
+from sklearn.tree import DecisionTreeClassifier
 import utils
 import datetime as dt
 
@@ -72,6 +73,72 @@ def svm_model_fit(
         print(f"Path of best model: {model_path}")
         return model_path
 
+
+def dtree_model_fit(
+        X_train,
+        X_test,
+        X_val,
+        y_train,
+        y_test,
+        y_val,
+        hyper_param_set={}
+):
+        assert len(hyper_param_set) > 0
+        h_param_names = list(hyper_param_set.keys())
+        list_of_hparams = [hyper_param_set[h_param] for h_param in h_param_names]
+        result_df = pd.DataFrame(columns=["train", "dev", "test"])
+        best_acc = -np.inf
+        best_model = None
+        best_hyper_params = None
+        best_row_idx = None
+        for h_param in list(product(*list_of_hparams)):
+                hypam_dict = {k:v for k, v in zip(h_param_names, h_param)}
+                print(hypam_dict)
+                d_clf = DecisionTreeClassifier()
+                d_clf.set_params(**hypam_dict)
+                d_clf.fit(X_train, y_train)
+                train_acc = d_clf.score(X_train, y_train)
+                dev_acc = d_clf.score(X_val, y_val)
+                test_acc = d_clf.score(X_test, y_test)
+                cur_row_idx = str(hypam_dict)
+                result_df.loc[cur_row_idx] = [train_acc, dev_acc, test_acc]
+                if dev_acc > best_acc:
+                        best_acc = dev_acc
+                        best_hyper_params = hypam_dict
+                        best_model = d_clf
+                        best_row_idx = cur_row_idx
+
+        # Converting the accuracy into percentage and printing the result
+        print(f"Printing the results:")
+        result_df = result_df * 100
+        print(result_df)
+        print("\n")
+
+        for col_name in result_df.columns:
+                col_min, col_max, col_mean, col_median = utils.get_min_max_mean_median(result_df[col_name])
+                print(f"Column : {col_name}")
+                print(f"\tMinimum Accuracy: {col_min}")
+                print(f"\tMaximum Accuracy: {col_max}")
+                print(f"\tMean Accuracy: {col_mean}")
+                print(f"\tMedian Accuracy: {col_median}")
+                
+
+        print(f"\nBest set of Hyper Parameters {best_hyper_params} as per dev set\n")
+        print(f"Results obtained with {best_hyper_params}:")
+        print(f"{result_df.loc[best_row_idx]}")
+        print("---------------------------------------------------------- \n")
+
+        best_model_file_name = f"dtree_digit_clf_"
+        for key in best_hyper_params:
+                best_model_file_name += f"{key}:{best_hyper_params[key]}_"
+        best_model_file_name += ".pkl"
+        model_path = os.path.join(MODEL_DIR, best_model_file_name)
+        utils.save_object(best_model, model_path)
+        print(f"Path of best model: {model_path}")
+        return model_path
+
+
+
 if __name__ == "__main__":
         
         TRAIN_SPLIT = 0.8
@@ -104,5 +171,12 @@ if __name__ == "__main__":
 
         model_path = svm_model_fit(X_train, X_val, X_test, y_train, y_val, y_test, C, GAMMA)
         print(model_path)
-        
+
+        h_param_set = {
+                "max_depth": [2, 4, 8, None],
+                "min_samples_split": [2, 8, 12],
+                "min_impurity_decrease": [0.0, 0.01, 0.001]
+        }
+        d_model_path = dtree_model_fit(X_train, X_val, X_test, y_train, y_val, y_test, h_param_set)
+        print(d_model_path)
         
