594a22e9e7da2b182d4863c3d686cc734743ebd1
souvikpal7 <pal.18@iitj.ac.in>
2022-11-19 21:57:51 +0530

    actions added

--- test_codes/test_utils.py
+++ test_codes/test_utils.py
@@ -4,10 +4,29 @@ from sklearn import svm
 # import sys
 # sys.path.append("..")
 import hand_written_digits
+import utils
 
 
 def test_saved_model_type():
-    model_path = hand_written_digits.svm_model_fit()
+    data, labels = utils.get_data_and_labels("digits")
+    #flattening the image
+    n_samples = len(data)
+    data = data.reshape((n_samples, -1))
+    TRAIN_SPLIT = 0.8
+    VALID_SPLIT = 0.1
+    TEST_SPLIT = 0.1
+    X_train, X_val, X_test, y_train, y_val, y_test = utils.get_train_dev_test_split(
+        data,
+        labels,
+        TRAIN_SPLIT,
+        VALID_SPLIT,
+        TEST_SPLIT
+    )
+
+    model_path, _ = hand_written_digits.svm_model_fit(
+        X_train, X_val, X_test, 
+        y_train, y_val, y_test
+    )
     assert os.path.exists(model_path), "Model does not exist at returned path"
 
     # load the model
